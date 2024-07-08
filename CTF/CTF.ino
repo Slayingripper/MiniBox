db799e1a2b02cdab41a3d4e759afff5750d0b1af
slayingripper <kirav96@gmail.com>
2024-07-08 16:42:49 +0100

    Update 0.2

--- CTF/CTF.ino
+++ CTF/CTF.ino
@@ -2,9 +2,31 @@
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
 #include <WiFiUdp.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 #include "Challenges.h"  // Include the challenges header file
 
-const char *ssid = "ESP8266_Honeypot";  // Set your SSID
+const char* spinnerFrames[] = {
+  "[      ]",   // Frame 0
+  "[*     ]",   // Frame 1
+  "[**    ]",   // Frame 2
+  "[***   ]",   // Frame 3
+  "[ ***  ]",   // Frame 4
+  "[  *** ]",   // Frame 5
+  "[   ***]",   // Frame 6
+  "[    **]",   // Frame 7
+  "[     *]"    // Frame 8
+};
+
+const int buzzerPin = D5;
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+const char *ssid = "MiniBox";  // Set your SSID
 char password[9];  // WiFi password, up to 8 characters plus null terminator
 
 // Set LED, switch, and buzzer pins
@@ -24,21 +46,46 @@ const int melody[] = {660, 660, 660, 510, 660, 770};
 const int noteDurations[] = {4, 4, 4, 8, 4, 4};
 
 // 3-way switch pin
-const int switchPin = D1;
+const int switchPin = D6;
+
+ChallengeLevel currentLevel;
+
+// Username and password for basic authentication
+char http_username[6] = "admin"; // Mutable character array for username
+char http_password[9]; // Mutable character array for password
+
 
 void setup() {
   // Initialize serial communication
   Serial.begin(115200);
-  delay(10);
-
+  delay(1000);
+  
+  // Initialize the display 
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  animateSpinner();
+  display.clearDisplay();
+  //display welcome message
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  //center the text
+  display.setCursor((SCREEN_WIDTH - 64) / 2, SCREEN_HEIGHT / 2 - 10);
+  // make text bigger
+  display.setTextSize(2);
+  display.println("MiniBox");
+  display.display();
+  delay(2000);
   // Set up LED pins
   pinMode(builtInLed, OUTPUT);
-  pinMode(apiLedPin, OUTPUT);
+  pinMode(apiLedPin, OUTPUT); // Ensure API LED pin is set as output
   pinMode(errorLedPin, OUTPUT);
+  pinMode(buzzerPin, OUTPUT); // Set the buzzer pin as output
 
-  digitalWrite(builtInLed, LOW); // Turn off built-in LED (assuming active low)
-  digitalWrite(apiLedPin, LOW); // Ensure API LED is off initially
-  digitalWrite(errorLedPin, LOW); // Ensure error LED is off initially
+  digitalWrite(builtInLed, HIGH); // Turn off built-in LED (assuming active low)
+  digitalWrite(apiLedPin, HIGH); // Ensure API LED is off initially
+  digitalWrite(errorLedPin, HIGH); // Ensure error LED is off initially
 
   // Set up switch pin
   pinMode(switchPin, INPUT);
@@ -64,72 +111,98 @@ void setup() {
   Serial.println(password);
 
   // Set up the web server based on challenge level
-  ChallengeLevel currentLevel = getCurrentChallengeLevel(switchPin);
+  currentLevel = getCurrentChallengeLevel(switchPin);
   setupServer(currentLevel);
 
   // Start the UDP server
   udp.begin(localUdpPort);
+  // display initial information
+  displayInfo();
 }
 
 void loop() {
   dnsServer.processNextRequest();
   server.handleClient();
   checkUdpPacket();
+  displayInfo();
 }
 
 void setupServer(ChallengeLevel level) {
   // Set up the web server based on challenge level
-  server.on("/", handleRoot);
+  server.on("/", HTTP_GET, [](){
+    if (!server.authenticate(http_username, http_password)) {
+      return server.requestAuthentication();
+    }
+    handleRoot();
+  });
 
   switch (level) {
     case EASY:
       // Allow easy access to API endpoints
-      server.on("/api/led/on", HTTP_GET, handleLedOn);
-      server.on("/api/led/off", HTTP_GET, handleLedOff);
-      server.on("/api/clients", HTTP_GET, handleClientList);
-      server.on("/api/scan", HTTP_GET, handleScanNetworks);
-      server.on("/api/password", HTTP_GET, handleShowPassword);
-      server.on("/api/test/buzzer", HTTP_GET, handleTestBuzzer);
+      server.on("/api/led/on", HTTP_GET, [](){
+        if (!server.authenticate(http_username, http_password)) {
+          return server.requestAuthentication();
+        }
+        handleLedOn();
+      });
+      server.on("/api/led/off", HTTP_GET, [](){
+        if (!server.authenticate(http_username, http_password)) {
+          return server.requestAuthentication();
+        }
+        handleLedOff();
+      });
+      server.on("/api/clients", HTTP_GET, [](){
+        if (!server.authenticate(http_username, http_password)) {
+          return server.requestAuthentication();
+        }
+        handleClientList();
+      });
+      server.on("/api/scan", HTTP_GET, [](){
+        if (!server.authenticate(http_username, http_password)) {
+          return server.requestAuthentication();
+        }
+        handleScanNetworks();
+      });
+      server.on("/api/password", HTTP_GET, [](){
+        if (!server.authenticate(http_username, http_password)) {
+          return server.requestAuthentication();
+        }
+        handleShowPassword();
+      });
+      server.on("/api/test/buzzer", HTTP_GET, [](){
+        if (!server.authenticate(http_username, http_password)) {
+          return server.requestAuthentication();
+        }
+        handleTestBuzzer();
+      });
       break;
 
     case MEDIUM:
-      // Add some restrictions or additional challenge
-      server.on("/api/led/on", HTTP_GET, handleChallengeLedOn);
-      server.on("/api/led/off", HTTP_GET, handleChallengeLedOff);
-      server.on("/api/clients", HTTP_GET, handleChallengeClientList);
-      server.on("/api/scan", HTTP_GET, handleChallengeScanNetworks);
-      server.on("/api/password", HTTP_GET, handleChallengeShowPassword);
-      server.on("/api/test/buzzer", HTTP_GET, handleTestBuzzer); // Allow buzzer testing
+      // Randomize password
+      generateRandomPassword(password, 8);
       break;
 
     case HARD:
-      // Implement more complex challenges or stricter access control
-      server.on("/api/led/on", HTTP_GET, handleChallengeLedOn);
-      server.on("/api/led/off", HTTP_GET, handleChallengeLedOff);
-      server.on("/api/clients", HTTP_GET, handleChallengeClientList);
-      server.on("/api/scan", HTTP_GET, handleChallengeScanNetworks);
-      server.on("/api/password", HTTP_GET, handleChallengeShowPassword);
-      server.on("/api/test/buzzer", HTTP_GET, handleTestBuzzer); // Allow buzzer testing
+      // Randomize username and password
+      generateRandomUsername(http_username, 5); // Generate a random username with 5 characters
+      generateRandomPassword(password, 8); // Generate a random password with 8 characters
       break;
 
     default:
-      // Default to easy level if switch position is unrecognized
-      server.on("/api/led/on", HTTP_GET, handleLedOn);
-      server.on("/api/led/off", HTTP_GET, handleLedOff);
-      server.on("/api/clients", HTTP_GET, handleClientList);
-      server.on("/api/scan", HTTP_GET, handleScanNetworks);
-      server.on("/api/password", HTTP_GET, handleShowPassword);
-      server.on("/api/test/buzzer", HTTP_GET, handleTestBuzzer);
       break;
   }
+
+  // Set up other routes as needed
 
   server.onNotFound(handleNotFound);
   server.begin();
   Serial.println("Server started");
 }
 
+
+
 void handleRoot() {
-  String html = "<!DOCTYPE HTML><html><h1>ESP8266 Honeypot</h1>";
+  String html = "<!DOCTYPE HTML><html><h1>MiniBox CTF Trainer</h1>";
   html += "<p><a href=\"/api/led/on\"><button>Turn On LED</button></a></p>";
   html += "<p><a href=\"/api/led/off\"><button>Turn Off LED</button></a></p>";
   html += "<p><a href=\"/api/clients\"><button>Show Connected Clients</button></a></p>";
@@ -141,12 +214,12 @@ void handleRoot() {
 }
 
 void handleLedOn() {
-  digitalWrite(apiLedPin, HIGH); // Turn the API LED on
+  digitalWrite(apiLedPin, LOW); // Turn the API LED on
   server.send(200, "application/json", "{\"message\":\"API LED is ON\"}");
 }
 
 void handleLedOff() {
-  digitalWrite(apiLedPin, LOW); // Turn the API LED off
+  digitalWrite(apiLedPin, HIGH); // Turn the API LED off
   server.send(200, "application/json", "{\"message\":\"API LED is OFF\"}");
 }
 
@@ -180,7 +253,7 @@ void handleScanNetworks() {
   int n = WiFi.scanNetworks();
   String response = "{\"network_count\":" + String(n) + ",\"networks\":[";
   for (int i = 0; i < n; ++i) {
-    response += "{\"ssid\":\"" + WiFi.SSID(i) + "\",\"channel\":" + String(WiFi.channel(i)) + "}";
+    response += "{\"ssid\":\"" + WiFi.SSID(i) + "\",\"rssi\":" + String(WiFi.RSSI(i)) + "}";
     if (i < n - 1) response += ",";
   }
   response += "]}";
@@ -193,44 +266,45 @@ void handleShowPassword() {
 }
 
 void handleChallengeLedOn() {
-  // Implement challenge logic or restrict access further
   server.send(403, "application/json", "{\"message\":\"Access Denied\"}");
 }
 
 void handleChallengeLedOff() {
-  // Implement challenge logic or restrict access further
   server.send(403, "application/json", "{\"message\":\"Access Denied\"}");
 }
 
 void handleChallengeClientList() {
-  // Implement challenge logic or restrict access further
   server.send(403, "application/json", "{\"message\":\"Access Denied\"}");
 }
 
 void handleChallengeScanNetworks() {
-  // Implement challenge logic or restrict access further
   server.send(403, "application/json", "{\"message\":\"Access Denied\"}");
 }
 
 void handleChallengeShowPassword() {
-  // Implement challenge logic or restrict access further
   server.send(403, "application/json", "{\"message\":\"Access Denied\"}");
 }
 
 void handleTestBuzzer() {
   // Play Super Mario mushroom sound for the first 3 seconds
   unsigned long startTime = millis();
+  Serial.println("Starting buzzer test...");
   for (int i = 0; i < sizeof(melody) / sizeof(melody[0]); i++) {
     // Check if more than 3 seconds have passed
     if (millis() - startTime > 3000) {
       break;
     }
     int toneDuration = 1000 / noteDurations[i];
-    tone(apiLedPin, melody[i], toneDuration); // Use apiLedPin for buzzer sound
+    Serial.print("Playing tone: ");
+    Serial.print(melody[i]);
+    Serial.print(" for duration: ");
+    Serial.println(toneDuration);
+    tone(buzzerPin, melody[i], toneDuration); // Use buzzerPin for buzzer sound
     delay(toneDuration * 1.30); // Pause between notes
-    noTone(apiLedPin); // Stop tone
+    noTone(buzzerPin); // Stop tone
   }
-  
+  Serial.println("Buzzer test completed.");
+
   server.send(200, "application/json", "{\"message\":\"Buzzer tested\"}");
 }
 
@@ -240,14 +314,6 @@ void handleNotFound() {
   digitalWrite(errorLedPin, HIGH);
 
   server.send(404, "application/json", "{\"message\":\"Not Found\"}");
-  logRequest();
-}
-
-void logRequest() {
-  Serial.print("Request from ");
-  Serial.print(server.client().remoteIP());
-  Serial.print(": ");
-  Serial.println(server.uri());
 }
 
 void checkUdpPacket() {
@@ -312,6 +378,14 @@ void generateRandomPassword(char *password, int length) {
   password[length] = '\0';  // Null-terminate the string
 }
 
+void generateRandomUsername(char *username, int length) {
+  const char alphanum[] = "abcdefghijklmnopqrstuvwxyz0123456789";
+  for (int i = 0; i < length; i++) {
+    username[i] = alphanum[random(36)];
+  }
+  username[length] = '\0';  // Null-terminate the string
+}
+
 ChallengeLevel getCurrentChallengeLevel(int switchPin) {
   // Determine the current challenge level based on switch position
   if (digitalRead(switchPin) == HIGH) {
@@ -320,5 +394,71 @@ ChallengeLevel getCurrentChallengeLevel(int switchPin) {
     // Add more conditions or checks for different challenge levels
     // For simplicity, assume MEDIUM level when switch is LOW
     return MEDIUM;
+  }
+}
+
+void displayInfo() {
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  
+  display.setCursor(0, 0);
+  display.print("Clients: ");
+  display.print(WiFi.softAPgetStationNum());
+
+  //display IP address and ap password
+  display.setCursor(0, 20);
+  display.print("AP IP: ");
+  display.println(WiFi.softAPIP());
+  display.setCursor(0, 30);
+  display.print("Password: ");
+  display.println(password);
+
+  
+  display.setCursor(0, 10);
+  display.print("Difficulty: ");
+  switch (currentLevel) {
+    case EASY:
+      display.print("Easy");
+      break;
+    case MEDIUM:
+      display.print("Medium");
+      break;
+    case HARD:
+      display.print("Hard");
+      break;
+    default:
+      display.print("Unknown");
+      break;
+  }
+  //display the username and password
+  display.setCursor(0, 40);
+  display.print("Username: ");
+  display.println(http_username);
+  display.setCursor(0, 50);
+  display.print("Password: ");
+  display.println(http_password);
+
+  
+  display.display();
+}
+
+void animateSpinner() {
+
+  const int numFrames = sizeof(spinnerFrames) / sizeof(spinnerFrames[0]);
+  const int frameDelay = 200;  // Adjust delay as needed for animation speed
+
+  for (int i = 0; i < numFrames; i++) {
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(0, 0);
+    display.println("Booting...");
+
+    display.setCursor(30, 30);  // Adjust position for the spinner
+    display.println(spinnerFrames[i]);
+    display.display();
+
+    delay(frameDelay);
   }
 }
